60523804b06f93fde024072df30b9534b4492c50
Simon Pratt <me@simondavidpratt.com>
2023-08-06 15:17:50 -0600

    Remove IPC

--- frontend/resume-desktop/src/main/preload.ts
+++ frontend/resume-desktop/src/main/preload.ts
@@ -1,27 +1,10 @@
 // Disable no-unused-vars, broken for spread args
 /* eslint no-unused-vars: off */
-import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
-
-export type Channels = 'ipc-example';
+import { contextBridge } from 'electron';
 
 const electronHandler = {
-  ipcRenderer: {
-    sendMessage(channel: Channels, ...args: unknown[]) {
-      ipcRenderer.send(channel, ...args);
-    },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
-      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-        func(...args);
-      ipcRenderer.on(channel, subscription);
-
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
-    },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
-      ipcRenderer.once(channel, (_event, ...args) => func(...args));
-    },
-  },
+    // Deliberately left empty
+    // Put any IPC handlers here
 };
 
 contextBridge.exposeInMainWorld('electron', electronHandler);
